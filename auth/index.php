c4a960eb16dbbd5573332859aa1f5c3d9a5aaab4
otisnth <otisnth@gmail.com>
2022-11-16 14:15:36 +0300

    создана форма авторизации

---
+++ auth/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+        <input type="email" name="email" require>
+        <input type="password" name="password" require pattern="[a-zA-Z0-9\-]+">
+        <button type="submit">Зарегистрироваться</button>
+    </form>
+</body>
+</html>
