ac064815a53b307065dda9ab0253af957f5495e9
otisnth <otisnth@gmail.com>
2022-11-16 14:35:34 +0300

    добавлена структура шаблонов

--- auth/index.php
+++ auth/index.php
@@ -1,11 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+
+<?php
+    require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/templates/main/header.php');
+?>
 <body>
     <form action="#" method="post">
         <input type="email" name="email" require>
