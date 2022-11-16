02b0aa3b9dae9cda9dda3ea40d72ee2e83591b65
otisnth <otisnth@gmail.com>
2022-11-16 14:45:57 +0300

    определен конфиг

--- auth/index.php
+++ auth/index.php
@@ -1,6 +1,7 @@
 
 <?php
-    require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/templates/main/header.php');
+    require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/init.php');
+    require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/templates/' . $config['template'] . '/header.php');
 ?>
 <body>
     <form action="#" method="post">
