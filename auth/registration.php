9dbc1830c5d567ee164cfab3c845224a059d1b42
lolipopmari <mit.maria2019@yandex.ru>
2022-11-17 13:06:27 +0300

    добавлена форма регистрации и js обработчик

--- auth/registration.php
+++ auth/registration.php
@@ -1,5 +1,6 @@
 <?php
-
+require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/init.php');
+require_once ($_SERVER['DOCUMENT_ROOT']. '/admin/templates/' . $config['template'] . '/header.php');
 /*
 ****** обработчик готов
 ****** url api = /admin/api/user/reg
@@ -13,3 +14,14 @@ email - email
 name - имя
 photo - фото
 */
+?>
+<form action="/admin/api/user/reg" method="post" enctype="multipart/form-data" class="form-reg">
+    <input type="email" name="email">
+    <input type="password" name="password" pattern="[a-zA-Z0-9\-]+">
+    <input type="password" name="password-repeat" pattern="[a-zA-Z0-9\-]+">
+    <input type="text" name="name" pattern="[а-яА-Я]+">
+    <input type="file" name="photo" accept="image/*">
+    <button type="submit">Зарегистрироваться</button>
+    <div class="error-block"></div>
+</form>
+<script src="/admin/assets/js/reg.js"></script>
